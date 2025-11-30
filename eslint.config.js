c8834db0558aafbca560e8bf2979eff128e11849
nun33 <nouraaljohani3@gmail.com>
2025-11-30 18:34:14 +0000

    config changes

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    files: ["assets/**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      env: { browser: true }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  }
+];
